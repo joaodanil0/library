793df1f583ae0249939fc33e452462466369e41c
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-24 07:15:14 -0400

    Creating user view

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from user import views
 
-urlpattern = [
-    path("login/", views.login)
+urlpatterns = [
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
