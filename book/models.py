66bf10267eeead4f74ef92d22dd593d10f940785
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-24 07:03:29 -0400

    Fixing Book Model

--- book/models.py
+++ book/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=30)
-    co_author = models.CharField(max_length=30)
-    register_date = models.DateField()
+    co_author = models.CharField(max_length=30, blank=True)
+    register_date = models.DateField(default=date.today)
     borrowed = models.BooleanField(default=False)
-    borrower_name = models.CharField(max_length=30)
-    borrow_date = models.DateTimeField()
-    borrow_return = models.DateTimeField()
-    borrow_time = models.DateField()
+    borrower_name = models.CharField(max_length=30, blank=True)
+    borrow_date = models.DateTimeField(blank=True, null=True)
+    borrow_return = models.DateTimeField(blank=True, null=True)
+    borrow_time = models.DateField(blank=True, null=True)
