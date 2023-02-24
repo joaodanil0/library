5e11d918b7a8cc3a04ac3a83fc73767060013179
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-24 08:45:01 -0400

    Adding user to DB

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from user.models import User
 
 # Create your views here.
 
@@ -13,4 +14,7 @@ def register_validation(request):
     name = request.POST.get("name")
     pwd = request.POST.get("password")
     email = request.POST.get("email")
+
+    user = User(name=name, password=pwd, email=email)
+    user.save()
     return HttpResponse(f'{name} {pwd} {email}')
