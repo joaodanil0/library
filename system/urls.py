24b7316f422e8b26e8ede61ee324720001145de7
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-25 12:32:47 -0400

    Creating System view

--- system/urls.py
+++ system/urls.py
@@ -3,4 +3,5 @@ from system import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('book_info/<int:id>', views.book_info, name="book_info"),
 ]
--- system/views.py
+++ system/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from user.models import User
+from book.models import Book
 
 # Create your views here.
 
@@ -10,6 +11,11 @@ def home(request):
     session = request.session.get('usr')
     if session:
         user = User.objects.get(id=session)
-        return HttpResponse(f"System Hello {user.name}")
+        book = Book.objects.filter(user=user)
+        return render(request, 'home.html', {'books': book})
     else:
         return redirect('/user/login/?status=2')
+
+def book_info(request, id):
+    book = Book.objects.get(id=id)
+    return render(request, 'book_info.html', {'book': book})
